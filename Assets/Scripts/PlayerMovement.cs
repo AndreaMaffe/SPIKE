28a55e85674821539591c9ea1a51341e700f3b43
francescomaiolani <francesco.maiolani@mail.polimi.it>
2018-10-27 17:57:22 +0200

    piccole correzioni bomba

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour {
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
 
     public float horizVelocity;
     public float maxVelocity;
 
-
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-	}
-	
 	// Update is called once per frame
 	void Update () {
         rb.AddForce(new Vector3(horizVelocity, 0, 0), ForceMode2D.Force);
+        if (rb.velocity.x >= maxVelocity)
+            rb.velocity = new Vector2(maxVelocity, rb.velocity.y);
 	}
 }
