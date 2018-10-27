36e3a4eb88f98a2aec871bdb0492362c9c3d72d8
francescomaiolani <francesco.maiolani@mail.polimi.it>
2018-10-27 12:18:40 +0200

    Inizializzazione scriptable object
    -Non toccare gli elementi nella cartella editor che tanto sono standard e servono solo a unity.
    -Modificare solo la classe Level che sara' la matrice degli scriptable object dei livelli

---
+++ Assets/Editor/LevelData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class LevelData
+{
+    [MenuItem("Assets/Create/Level")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<Level>();
+    }
+}
+
