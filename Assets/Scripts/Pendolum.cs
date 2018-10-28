d07893c46949bafd86921c87b1484da8cc1f1a8b
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2018-10-28 16:40:47 +0100

    Pendolum
    aggiunto Prefab Pendolum

---
+++ Assets/Scripts/Pendolum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendolum : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public float oscillation;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector3(oscillation, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
