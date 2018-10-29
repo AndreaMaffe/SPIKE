d2561f0afabb5fab2d9094555bb3b70128ab9043
francescomaiolani <francesco.maiolani@mail.polimi.it>
2018-10-29 13:46:01 +0100

    Bottoni ostacoli

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
@@ -9,17 +10,50 @@ public class LevelManager : MonoBehaviour {
     //numero del livello attuale partendo da 1
     private int currentLevel;
 
+    [SerializeField]
+    public ObstacleData[] testObstacle;
+
+    public GameObject obstacleButton;
+    public int buttonInBetweenSpace;
+    public int buttonWidth;
+    public Transform buttonPanel; 
+
     private void Start()
     {
         LoadLevel();
     }
 
     void LoadLevel() {
-
+        CreateUIObstacleButtons();
     }
 
     public Level GetActualLevel() {
         //i livelli partono da 1
         return allLevels[currentLevel - 1];
     }
+
+    void CreateUIObstacleButtons() {
+
+        for (int i = 0; i < testObstacle.Length; i++) {
+
+            Vector2 buttonLocalPosition;
+            //A seconda se i buttoni sono pari o dispari li posiziona nel modo giusto
+            if (testObstacle.Length % 2 == 1)
+                buttonLocalPosition = new Vector2(-(buttonWidth + buttonInBetweenSpace) * (testObstacle.Length / 2)+ i * (buttonWidth + buttonInBetweenSpace), 0);
+            else
+                buttonLocalPosition = new Vector2(-((buttonWidth + buttonInBetweenSpace)/2 + (testObstacle.Length / 2 - 1) * (buttonWidth + buttonInBetweenSpace)) + i * (buttonWidth + buttonInBetweenSpace), 0);
+
+            //crea il bottone come figlio del panel giusto
+            GameObject button = Instantiate(obstacleButton, buttonPanel, false);
+            //posiziona il bottone
+            button.GetComponent<RectTransform>().localPosition = buttonLocalPosition;
+            button.GetComponent<ObstacleButton>().AssignObstacleTypeAndAmount(testObstacle[i].obstacleName, testObstacle[i].obstacleMaxAmount);
+        }
+    }
+}
+
+[System.Serializable]
+public struct ObstacleData {
+    public string obstacleName;
+    public int obstacleMaxAmount;
 }
---
+++ Assets/Scripts/UI/ObstacleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ObstacleButton : MonoBehaviour {
+
+    private string obstacleName;
+    private int obstacleAmount;
+
+    public Image obstacleImage;
+    public TextMeshProUGUI obstacleAmountText;
+
+    public void AssignObstacleTypeAndAmount(string type, int amount) {
+        this.obstacleName = type;
+        this.obstacleAmount = amount;
+        AssignUIValues();
+    }
+
+    void AssignUIValues() {
+        obstacleImage.sprite = Resources.Load<Sprite>("UIObstaclesImages/" + obstacleName);
+        Debug.Log("UIObstaclesImages/" + obstacleName);
+        obstacleAmountText.text = "x " + obstacleAmount;
+    }
+}
