e15742f76c397b33b8a26ae0f5cd18883ecee791
francescomaiolani <francesco.maiolani@mail.polimi.it>
2018-10-30 10:46:17 +0100

    AnchorPoints

---
+++ Assets/AnchorPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnchorPointPosition { Top, Side, Platform };
+
+public class AnchorPoint : MonoBehaviour {
+ 
+    [Header("Position can be: top, side o platform")]
+    public AnchorPointPosition position;
+    public bool edge;
+    [SerializeField]
+    private bool occupied;
+
+    public void SetOccupied(bool occupied) {
+        this.occupied = occupied;
+    }
+
+    public bool GetOccupied() {
+        return occupied;
+    }
+
+    public void SetPosition(AnchorPointPosition position) {
+        this.position = position; 
+    }
+
+    public void SetEdge(bool edge) {
+        this.edge = edge;
+    }
+   
+
+    
+}
