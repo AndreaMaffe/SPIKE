fbf4e0b72aa834525e6d2dbadee0d66207fce342
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2018-10-30 11:43:02 +0100

    Obstacle class
    aggiunta classe Obstacle e Game Manager, modificato Level Manager

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    private bool active;
+
+	// Use this for initialization
+	void Start () {
+        active = false;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (active)
+            UpdateObstacle();
+    }
+
+    void UpdateObstacle() { 
+
+    }
+
+    public void WakeUp() {
+
+    }
+
+    public void SetActive() {
+        active = true;
+    }
+
+}
