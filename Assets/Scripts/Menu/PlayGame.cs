dad6b7e2ef3e8096c2f30284beb3d661bc73ac08
pierstefano bellani <pierstefano.bellani@gmail.com>
2018-11-03 15:00:18 +0100

    main menu
    main menu

---
+++ Assets/Scripts/Menu/PlayGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayGame : MonoBehaviour {
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
