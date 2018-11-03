0062feef9187b8e8432a2bc5c4d081e660448554
pierstefano bellani <pierstefano.bellani@gmail.com>
2018-11-03 16:11:37 +0100

    animation menu pt1

---
+++ Assets/Scripts/Menu/MenuTransition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTransition : MonoBehaviour {
+
+	public void RubberTransition()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
