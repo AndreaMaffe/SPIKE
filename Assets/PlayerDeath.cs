8af86fed705029e8871dd7c55ef9c5b309dbccb5
francescomaiolani <francesco.maiolani@mail.polimi.it>
2018-11-05 13:29:41 +0100

    Scena piu' grande e ragdoll

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    public Animator animator;
+    public Rigidbody2D[] RagdollPieces;
+    public Collider2D[] RagdollColliders;
+
+    [Header("Il rigidbody e il collider principali")]
+    public Rigidbody2D mainRigidbody;
+    public BoxCollider2D mainCollider;
+	
+    public void ActivateRagdoll()
+    {
+        animator.enabled = false;
+        mainRigidbody.simulated = false;
+        mainCollider.enabled = false;
+        foreach (Rigidbody2D rb in RagdollPieces)
+        {
+            rb.simulated = true;
+        }
+        foreach (Collider2D col in RagdollColliders)
+        {
+            col.enabled = true;
+        }
+    }
+}
