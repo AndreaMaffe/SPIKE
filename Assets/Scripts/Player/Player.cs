0ac2645467407db76b1426db000e162ed74ef97f
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2018-11-20 14:23:10 +0100

    Piccole modifiche

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -67,7 +67,7 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         //se tocca una piattaforma
-        if (Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y), Vector2.down, 0.1f, LayerMask.GetMask("Platform")))
+        if (Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y), Vector2.down, 0.1f , LayerMask.GetMask("Platform")))
         {
             //al primo contatto, annulla la velocità e fai reiniziare il Player a correre
             if (!onGround)
