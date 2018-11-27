01142e2679f0754dee428ebbfd0820c3f9157bd9
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2018-11-27 13:13:41 +0100

    Modifiche varie

---
+++ Assets/Scripts/Obstacles/ElevatorPlatform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPlatform : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.parent.gameObject.GetComponent<Elevator>().InvertDirection();
+    }
+}
