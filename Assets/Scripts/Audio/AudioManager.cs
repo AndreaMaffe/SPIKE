8a2d6ba5e6be8333508d51d4d5f67ad0a356e947
pierstefano bellani <pierstefano.bellani@gmail.com>
2018-11-27 13:31:27 +0100

    audiomanager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
 
-    public Slider volumeSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
 
     bool IsInPlaymode = false;
 
     public void Start()
     {
         //Adds a listener to the main slider and invokes a method when the value changes.
-        volumeSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        musicSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+
     }
 
     // Invoked when the value of the slider changes.
     public void ValueChangeCheck()
     {
-        //Debug.Log(volumeSlider.value);
-        AudioListener.volume = volumeSlider.value;
+        //Debug.Log(musicSlider.value);
+        AudioListener.volume = musicSlider.value;
     }
 
     public void BuildPlaySoundtrack()
