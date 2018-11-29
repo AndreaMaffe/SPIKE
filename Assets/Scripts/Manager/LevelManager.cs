d9c61dd841ab227cfe9e3770a02debdc55c881c8
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2018-11-29 10:30:06 +0100

    PlayerSimulator opzionale

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -40,6 +40,8 @@ public class LevelManager : MonoBehaviour
     public Text currentTimeText;
     private float currentTime;
 
+    public bool playerSimulator;
+
 
     //struct che associa ogni ostacolo alla posizione in cui puo' andare
     [System.Serializable]
@@ -95,8 +97,8 @@ public class LevelManager : MonoBehaviour
         CreatePlatforms(level);
         CreateUIObstacleButtons(level);
         Instantiate(Resources.Load<GameObject>("Prefab/Player"), level.startingPoint, Quaternion.identity);
-        Instantiate(Resources.Load<GameObject>("Prefab/PlayerSimulator"), level.startingPoint, Quaternion.identity);
-
+        if (playerSimulator)
+            Instantiate(Resources.Load<GameObject>("Prefab/PlayerSimulator"), level.startingPoint, Quaternion.identity);
         Instantiate(Resources.Load<GameObject>("Prefab/Flag"), level.endingPoint, Quaternion.identity);
         currentTime = 0.0f;
     }
